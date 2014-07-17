1e822ec3ffc97dd2f588b3ec377d70da9441823a
thomasdavis <thomasalwyndavis@gmail.com>
2014-07-17 18:28:54 +1000

    screenshot

--- render.js
+++ render.js
@@ -1,6 +1,10 @@
 var webshot = require('webshot');
 var fs = require('fs');
 var html = fs.readFileSync('index.html', 'utf8');
-webshot('http://localhost/github/congress-pics/index.html', 'hello_world.png', {streamType: 'png', quality: 100}, function(err) {
+webshot('http://localhost/github/congress-pics/index.html', 'hello_world.png', 
+	{streamType: 'png', quality: 100,
+	windowSize:	
+{ width: 1600
+, height: 1050 }}, function(err) {
   // screenshot now saved to hello_world.png
 });
