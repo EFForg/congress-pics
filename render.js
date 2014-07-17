a440e0cb83469407ece929475e72fe46448c68c8
thomasdavis <thomasalwyndavis@gmail.com>
2014-07-17 20:00:24 +1000

    final touches

--- render.js
+++ render.js
@@ -5,6 +5,6 @@ webshot('http://localhost/github/congress-pics/index.html', 'full.png',
 	{streamType: 'png', quality: 100,
 	windowSize:	
 { width: 1600
-, height: 1050 }}, function(err) {
+, height: 1050 }, shotSize: {height: 'all'}}, function(err) {
   // screenshot now saved to hello_world.png
 });
